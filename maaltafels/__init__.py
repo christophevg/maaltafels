6edf1d159a2481e8b81eafe72fe498ed117f31f2
Christophe VG <contact@christophe.vg>
2020-08-13 14:16:23 +0200

    improved logging and connection string parsing

--- maaltafels/__init__.py
+++ maaltafels/__init__.py
@@ -1,9 +1,25 @@
-__version__ = "1.1.1"
-
-import logging
-logger = logging.getLogger(__name__)
+__version__ = "1.2.1"
 
 import os
+import logging
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL") or "DEBUG"
+
+logger = logging.getLogger(__name__)
+
+FORMAT  = "[%(asctime)s] [%(name)s] [%(process)d] [%(levelname)s] %(message)s"
+DATEFMT = "%Y-%m-%d %H:%M:%S %z"
+
+logging.basicConfig(level=logging.DEBUG, format=FORMAT, datefmt=DATEFMT)
+formatter = logging.Formatter(FORMAT, DATEFMT)
+
+# adjust gunicorn logger to global level and formatting 
+gunicorn_logger = logging.getLogger("gunicorn.error")
+gunicorn_logger.handlers[0].setFormatter(formatter)
+gunicorn_logger.setLevel(logging.getLevelName(LOG_LEVEL))
+
+logging.getLogger("gunicorn.error").setLevel(logging.INFO)
+logging.getLogger().handlers[0].setFormatter(formatter)
 
 from functools import wraps
 
@@ -11,11 +27,12 @@ from flask import Flask, request, Response
 
 from pymongo import MongoClient
 
-DB   = os.environ.get("MONGODB_URI", "mongodb://localhost:27017/maaltafels")
-
+DB       = os.environ.get("MONGODB_URI", "mongodb://localhost:27017/maaltafels")
 mongo    = MongoClient(DB)
 database = DB.split("/")[-1]
+if "?" in database: database = database.split("?")[0]
 db       = mongo[database]
+logger.info("connected to {} on {}".format(database, DB))
 
 server = Flask(__name__, static_url_path="/media")
 
